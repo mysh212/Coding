43d7d39430bc6a2f91566dbb5790f5973f218ab3
mysh212 <mysh212@gmail.com>
2022-07-02 14:19:29 +0800

    linux

--- linux/mc.cpp
+++ linux/mc.cpp
@@ -15,14 +15,20 @@ int main(int argc,char *args[]) {
         return 1;
     }
     bool c = 1;
-    if(exists_test3(args[1])) {
+    char y[10000];
+    string s;
+    s.assign(args[1]);
+    if(s.find(".cpp") != -1) s.replace(s.find(".cpp"),4,"");
+    strcat(y,s.c_str());
+    strcat(y,".cpp");
+    if(exists_test3(y)) {
         c = 0;
         if(!(argc == 3 && args[2][0] == 'S')) {
             cerr<<" File already existed.\n";
             return 1;
         }
     }
-    freopen(args[1],"w",stdout);
+    freopen(y,"w",stdout);
     cout<<"// Author : ysh\n// ";
     time_t now = time(0);
     tm *ltm = localtime(&now);
@@ -34,6 +40,6 @@ int main(int argc,char *args[]) {
     cout<<"#include<bits/stdc++.h>\nusing namespace std;\nint main() {\n    ios::sync_with_stdio(false);\n    cin.tie(0);\n\n    \n}";
     char x[10000];
     strcat(x,"code ");
-    strcat(x,args[1]);
+    strcat(x,y);
     system(x);
 }
