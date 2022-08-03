f03b59737329b1bedde56dc79bb1e17cef6e6479
mysh212 <mysh212@gmail.com>
2022-08-03 20:32:58 +0800

    ok

--- AP325-d064.cpp
+++ AP325-d064.cpp
@@ -2,18 +2,16 @@
 // 07/18/2022 Mon  9:39:51.29
 #include<bits/stdc++.h>
 using namespace std;
+struct tree{
+    tree* l;
+    tree* r;
+    int left,right;
+    int v;
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     int n;cin>>n;
-    map<int,int>m;
-    vector<int>f(n);
-    for(int i = 0;i<n;i++) {
-        cin>>f[i];
-    }
-    for(int i = n - 1;i>=0;i--) {
-        auto found = m.upper_bound(-f[i]);
-        
-    }
+    
 }
