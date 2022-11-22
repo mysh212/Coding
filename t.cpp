4cedd97b306a9d57435fbc1506fbd841f6becc59
mysh212 <82254324+mysh212@users.noreply.github.com>
2022-11-22 22:14:08 +0800

    ok

--- t.cpp
+++ t.cpp
@@ -1,12 +1,56 @@
 // Author : ysh
-// 11/18/2022 Fri 14:59:48.11
+// 11/20/2022 Sun 23:17:41.26
 #include<bits/stdc++.h>
 using namespace std;
-#include<square>
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    vector<vector<int>>f({1,1},{1,0});
-    t<int>r(f);
+    int n;cin>>n;
+    while(n--) {
+        int n;cin>>n;
+        vector<vector<int>>f(n);
+        // vector<bool>in(n),c(n);
+        vector<bool>to(n);
+        for(int i = 0;i<n;i++) {
+            for(int j = 0;j<n;j++) {
+                char tmp;cin>>tmp;
+                if(tmp == '1') {
+                    f.at(i).push_back(j);
+                    to.at(j) = 1;
+                }
+            }
+        }
+        vector<vector<bool>>ans(n,vector<bool>(n));
+        for(int i = 0;i<n;i++) ans[i][i] = 1;
+        vector<int>now;
+        function<void(int,int)> check = [&] (int last,int x) {
+            now.push_back(x);
+            for(int &i : now) ans.at(x).at(i) = 1;
+            for(int &i : f.at(x)) {
+                if(i != last) check(x,i);
+            }
+            now.pop_back();
+            return;
+        };
+        for(int i = 0;i<n;i++) {
+            if(to.at(i) == 0) check(-1,i);
+        }
+        for(int i = 0;i<n;i++) {
+            vector<int>tmp;
+            int c = 0;
+            for(int j = 0;j<n;j++) {
+                if(ans.at(i).at(j)) {
+                    c++;
+                    tmp.push_back(j);
+                }
+            }
+            cout<<c<<" ";
+            for(int i : tmp) {
+                cout<<i<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
 }
