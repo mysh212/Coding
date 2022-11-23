89377d3d187ea371616bc021c767bc7733b2fdb4
ysh <82254324+mysh212@users.noreply.github.com>
2022-11-23 17:26:44 +0800

    ok

--- Count-GCD.cpp
+++ Count-GCD.cpp
@@ -10,22 +10,27 @@ const int R = 998244353;
 #define debug(...) '*'
 #define printf(...) '*'
 #endif
-
+//#define int long long
+vector<bool>f(46341);
+vector<int>prime;
 vector<int> check(int x) {
     vector<int>f;
-    for(int i = 2;i<=x;i++) {
+    for(int &i : prime) {
+        if(x == 1 || i * i > x) break;
         if(x % i == 0) f.push_back(i);
         while(x % i == 0) {
             x = x / i;
         }
     }
+    if(x != 1) f.push_back(x);
     return f;
 }
-int main() {
-    // ios::sync_with_stdio(false);
-    // cin.tie(0);
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
     int n;cin>>n;
+    for(int i = 2;i<46341;i++) { if(!f[i]) { prime.push_back(i); for(int j = i * i;j<46341;j+=i) { f[j] = 1; } } }
     while(n--) {
         int a,b;cin>>a>>b;
         vector<int>f(a);
@@ -42,16 +47,29 @@ int main() {
             cout<<"0\n";
             continue;
         }
+        vector<int>fre(check(f.at(0)));
         function<int(int,int)> ck = [&] (int x,int t) {
             int rr = b;
             rr = rr / t;
-            vector<int>re(check(x));
-            debug(x,rr,re);
-            for(int &i : re) {
-                rr = rr - (rr / i);
+            vector<int>re;
+            for(int &i : fre) {
+                if(x % i == 0) re.push_back(i);
             }
-            debug(rr);
-            return rr;
+            int n = re.size();
+            long long ans = 0;
+            for(int i = 1,len = (1 << n);i<len;i++) {
+                long long now = 1;
+                int t = 0;
+                for(int j = 0;j<n;j++) {
+                    if((i >> j) & 1) {
+                        now = now * re.at(j);
+                        t++;
+                    }
+                }
+                if((t & 1)) ans = ans + (rr / now);
+                else ans = ans - (rr / now);
+            }
+            return rr - ans;
         };
         vector<int>v;
         for(int i = 1;i<a;i++) {
