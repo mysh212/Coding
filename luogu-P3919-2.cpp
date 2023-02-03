43e83c8781e5301af0ee5349c0f8055e0c638471
ysh <mysh212@gmail.com>
2023-02-03 15:10:57 +0800

    ok

--- luogu-P3919-2.cpp
+++ luogu-P3919-2.cpp
@@ -19,10 +19,10 @@ int update(int x,int v,int t = 0,int l = -1,int r = -1) {
     if(l == r) return create(v);
     int mid = (l + r) >> 1;
     if(x <= mid) return update(x,v,f.at(t).left,l,mid);
-    else return update(x,v,f.at(t).right,mid + 1,r),create(f.at(f.at(t).left),);
+    else return update(x,v,f.at(t).right,mid + 1,r);
 }
 
-int sum(int x,int r,int l = -1,int r = -1) {
+int sum(int x,int v,int l = -1,int r = -1) {
     if(l == -1 && r == -1) l = 0,r = n - 1;
 
 }
--- t.cpp
+++ t.cpp
@@ -1,18 +1,56 @@
 // Author : ysh
-// 02/03/2023 Fri  9:26:40.53
+// 02/03/2023 Fri 13:18:37.53
 #include<bits/stdc++.h>
 using namespace std;
-vector<int>color;
-inline void mg(int a,int b) {
+#ifdef LOCAL
+#include<debug.h>
+#else
+#define debug(...);
+#endif
 
+vector<int>f;
+vector<int>& check(string a) {
+    f.resize(a.size());
+    for(int i = 1,len = a.size();i<len;i++) {
+        int p = f[i - 1];
+        while(a[p] != a[i] && p != 0) p = f[p - 1];
+        if(a[p] == a[i]) f[i] = p + 1;
+    }
+    return f;
+}
+vector<int>v;
+vector<int>& ck(string a) {
+    v.resize(a.size());
+    for(int i = 2,len = a.size();i<len;i++) {
+        int p = f[i - 2];
+        while(a[p + 1] != a[i] && p != 0) p = f[p - 1];
+        if(a[p + 1] == a[i]) v[i] = p + 2;
+    }
+    return v;
 }
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int a,b,c;cin>>a>>b>>c;
-    vector<vector<int>>f(a);
-    for(int i = 0;i<b;i++) {
-
+    string a,b;cin>>a>>b;
+    auto v = check(b + " " + a);
+    auto vv = ck(b + " " + a);
+    debug(v);
+    debug(vv);
+    int ans = 0;
+    vector<int>aans;
+    for(int i = b.size() + 1,len = v.size();i<len;i++) {
+        if(v.at(i) == b.size() - 1 && len != v.size() - 1) ans++,aans.push_back(i - ((b.size() << 1) - 2));
+        if(v.at(i) == b.size()) ans++,aans.push_back(i - ((b.size() << 1) - 1));
     }
+    for(int i = b.size() + 1,len = v.size();i<len;i++) {
+        if(vv.at(i) == b.size()) ans++,aans.push_back(i - ((b.size() << 1) - 1));
+    }
+    sort(aans.begin(),aans.end());
+    aans.resize(unique(aans.begin(),aans.end()) - aans.begin());
+    cout<<aans.size()<<"\n";
+    for(int &i : aans) {
+        cout<<i<<" ";
+    }
+    return 0;
 }
