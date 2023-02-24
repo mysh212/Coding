7f181c7e2452c3dbbbde4bce1d82dea5fd3d7eae
ysh <mysh212@gmail.com>
2023-02-24 15:03:08 +0800

    ok

---
+++ Meet-in-the-Middle-2.cpp
@@ -0,0 +1,19 @@
+// Author : ysh
+// 02/24/2023 Fri 13:28:19.73
+// https://cses.fi/problemset/task/1628
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void check(int l,int r,set<int>&s,vector<int>&f,int x) {
+    
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int a,b;cin>>a>>b;
+    vector<int>f(a);
+    for(int &i : f) cin>>i;
+
+
+}
