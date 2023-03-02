bd9f9298342caddd42aef879daa05a6dee1bed2c
ysh <mysh212@gmail.com>
2023-03-02 13:19:16 +0800

    ok

---
+++ TIOJ-1171.cpp
@@ -0,0 +1,11 @@
+// Author : ysh
+// 03/02/2023 Thu  9:37:19.24
+// https://tioj.ck.tp.edu.tw/problems/1171
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+
+}
