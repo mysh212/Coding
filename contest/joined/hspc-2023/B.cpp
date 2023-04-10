d5b251597249cc8289da37704f93ef3441111f2f
ysh <82254324+mysh212@users.noreply.github.com>
2023-04-10 11:22:47 +0800

    ok

--- contest/joined/hspc-2023/B.cpp
+++ contest/joined/hspc-2023/B.cpp
@@ -6,57 +6,26 @@ struct box{
     int a,b,c;
     box(int a = 0,int b = 0,int c = 0):
         a(a), b(b), c(c) {};
-    bool operator<(const box &x) const {
+    bool operator<(box &x) {
         return c < x.c;
     }
 };
-// vector<int>color;
-// vector<int>now;
-// int ff(int x) {
-//     if(color.at(x) == x || color.at(x) == -1) return x;
-//     return color.at(x) = ff(color.at(x));
-// }
-// int r = 0;
-// bool mg(int a,int b) {
-//     int aa = ff(a);
-//     int bb = ff(b);
-//     int aaa = now.at(a);
-//     int bbb = now.at(b);
-//     if(!(aaa == -1 || bbb == -1 || (aaa == 1 && bbb == 0) || (aaa == 0 && bbb == 1))) return 0;
-//     if(aaa == bbb) aaa = 1,bbb = 0;
-//     color.at(aa) = bb;
-//     return !(r == 1);
-// }
-bool ok(vector<vector<int>>&f,vector<short>&mark,int x,bool r = 0) {
-    if(mark.at(x) != -1 && mark.at(x) != (r ? 1 : 0)) return 0;
-    if(mark.at(x) == (r ? 1 : 0)) return 1;
-    mark.at(x) = (r ? 1 : 0);
-    for(int &i : f.at(x)) {
-        if(!ok(f,mark,!r)) return 0;
-    }
-    return 1;
+vector<int>color;
+int ff(int x) {
+    if(color.at(x) == x || color.at(x) == -1) return x;
+    return color.at(x) = ff(color.at(x));
 }
-int check(int x,vector<box>&f,int a,int b) {
-    vector<vector<int>>v(a);
-    for(int i = 0;i<x;i++) {
-        int x = f.at(i).a;
-        int y = f.at(i).b;
-        v.at(x).push_back(y);
-        v.at(y).push_back(x);
-    }
-    vector<short>now(a);
-    for(int i = 0;i<a;i++) {
-        if(now.at(i) == -1) {
-            if(!ok(v,now,i,0)) return 0;
-        }
-    }
-    return 1;
+void mg(int a,int b) {
+    color.at(ff(a)) = ff(b);
+    return;
 }
-int ck(int l,int r,vector<box>&f,int a,int b) {
-    if(l == r) return l;
-    int mid = (l + r) >> 1;
-    if(check(mid,f,a,b)) return ck(l,mid,f,a,b);
-    else return ck(mid + 1,r,f,a,b);
+int check(vector<box>&f,int a,int b) {
+    color.resize(a << 1,-1);
+    for(auto &i : f) {
+        if(ff(i.a) == ff(i.b)) return i.c;
+        mg(i.a,i.b + a);mg(i.b,i.a + a);
+    }
+    return 0;
 }
 int main() {
     ios::sync_with_stdio(false);
@@ -64,12 +33,11 @@ int main() {
 
     int a,b;cin>>a>>b;
     vector<box>f(b);
-    for(box &i : f) {
+    for(auto &i : f) {
         cin>>i.a>>i.b>>i.c;
         i.a--;i.b--;
     }
     sort(f.rbegin(),f.rend());
-    int tmp = ck(0,b,f,a,b);
-    cout<<check(7,f,a,b);
+    cout<<check(f,a,b);
     return 0;
 }
