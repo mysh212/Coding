28e3265eb1ee39ea1db737b52aef909a0c168fd1
ysh <mysh212@gmail.com>
2023-09-28 15:30:07 +0800

    ok

--- Reverse-Madness.cpp
+++ Reverse-Madness.cpp
@@ -1,7 +1,6 @@
 // Author : ysh
 // 09/28/2023 Thu 13:54:12.69
 // https://codeforces.com/contest/1878/problem/D
-// NF
 #include<bits/stdc++.h>
 using namespace std;
 #ifdef LOCAL
@@ -19,7 +18,7 @@ int main() {
     while(n--) {
         int n,m;cin>>n>>m;
         string a;cin>>a;
-        vector<bool>f(n),mark(m);
+        vector<bool>f(n),mark(n);
         vector<pair<int,int>>v(m);
         for(auto &i : v) {
             cin>>i.first;
@@ -34,24 +33,21 @@ int main() {
         while(r--) {
             int tmp;cin>>tmp;
             tmp--;
-            auto found = lower_bound(v.rbegin(),v.rend(),make_pair(tmp,n - 1),[](pair<int,int>a,pair<int,int>b) {
-                return a.first > b.first;
-            });
-            // assert(next(found) != v.begin());
-            int now = (m - 1) - (found - v.rbegin());
-            // cerr<<"now: "<<now<<"\n";
-            mark.at(now) = !mark.at(now);
+            mark.at(tmp) = !mark.at(tmp);
         }
         debug(mark);
 
         for(int i = 0;i<m;i++) {
-            if(!mark.at(i)) {
-                for(int j = v.at(i).first;j<=v.at(i).second;j++) {
-                    cout<<a.at(j);
-                }
-                continue;
+            bool c = 0;
+            for(int j = v.at(i).first;j<=((v.at(i).first + v.at(i).second) >> 1);j++) {
+                int k = (v.at(i).first + v.at(i).second) - j;
+                if(c) swap(a.at(j),a.at(k));
+                if(mark.at(j)) swap(a.at(j),a.at(k));
+                if(mark.at(k)) swap(a.at(j),a.at(k));
+                c = c ^ mark.at(j) ^ mark.at(k);
+                cout<<a.at(j);
             }
-            for(int j = v.at(i).second;j>=v.at(i).first;j--) {
+            for(int j = ((v.at(i).first + v.at(i).second) >> 1) + 1;j<=v.at(i).second;j++) {
                 cout<<a.at(j);
             }
         }
