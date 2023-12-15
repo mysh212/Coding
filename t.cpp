313e28d0806c8481c6bc9e7f8d0e7a96c0335604
ysh <mysh212@gmail.com>
2023-12-15 16:46:27 +0800

    ok

--- t.cpp
+++ t.cpp
@@ -1,57 +1,145 @@
-// Author : ysh
-// 2023/12/12 Tue 16:12:39
-#include<bits/stdc++.h>
-using namespace std;
+
+//****************
+//電機一甲_412241080_張靖妍
+//****************
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<stdlib.h>
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+	int firstclass, businessclass, economyclass;  /*設為firstclass為頭等艙, 為businessclass為商務艙, 為economyclass為經濟艙*/
+	firstclass = 2, businessclass = 3, economyclass = 5;/*firstclass 有兩個座位 , businessclass有三個座位, economyclass有五個座位*/
+	int seat_class, seat_number; /*設seat_class為艙等, seat_number為座位編號*/
+	int a, b, c, d, e, f, g, h, i, j;/*設a為頭等艙-1, b為頭等艙-2, c為商務艙-1, d為商務艙-2, e為商務艙-3, f為經濟艙-1, g為經濟艙-2, h為經濟艙-3, i為經濟艙-4, j為經濟艙-5*/
+	a = b = c = d = e = f = g = h = i = j = 0;
+	while (1) {
+		scanf("%d", &seat_class);
+		if (seat_class == -1)
+			printf("(程式結束)\n");
+		if (seat_class < 1 || seat_class>3)
+			printf("X\n");
+	}
+	scanf("%d", &seat_number);
+	if (seat_class == 1 && seat_number > 0 && seat_number <= 2)
+		printf("S\n");
+	if (seat_class == 2 && seat_number > 0 && seat_number <= 3)
+		printf("S\n");
+	if (seat_class == 3 && seat_number > 0 && seat_number <= 5)
+		printf("S\n");
+	while (scanf("%d", &seat_number)) /*輸入1為頭等艙，輸入2為商務艙，輸入3為經濟艙*/
+	{
+	    if (seat_class == 1 && seat_number == 1)           /*成功則訂位並佔據該位置，失敗則跳出*/ 
+	    {             
+		    if (a)
+				printf("N\n"); 
+		    else 
+				printf("S\n");
+			printf("%d", firstclass);
+			firstclass = firstclass - 1;
+	    } 
+	    if (seat_class == 1 && seat_number == 2)
+	    {
+		    if (b)
+				printf("X\n"); 
+		    else
+				printf("S\n"); 
+			printf("%d", firstclass);
+			firstclass = firstclass - 1;
+	    }
+	    if (seat_class == 2 && seat_number == 1) 
+	    {
+		    if (c)
+				printf("X\n"); 
+		    else 
+				printf("S\n"); 
+			printf("%d", businessclass);
+			businessclass = businessclass- 1;
+	    }
+	    if (seat_class == 2 && seat_number == 2) 
+	    {
+		    if (d) 
+				printf("X\n"); 
+		    else 
+				printf("S\n");
+			printf("%d", businessclass);
+			businessclass = businessclass - 1;
+	    }
+	    if (seat_class == 2 && seat_number == 3)
+	    {
+		    if (e) 
+				printf("X\n");
+	        else
+				printf("S\n");
+			printf("%d", businessclass);
+			businessclass = businessclass - 1;
+	    }
+	    if (seat_class == 3 && seat_number == 1)
+	    {
+			if (f)
+				printf("X\n");
+		    else 
+				printf("S\n");
+			printf("%d", economyclass);  
+			economyclass = economyclass - 1; 
+	    }
+	    if (seat_class == 3 && seat_number == 2)
+	    { 
+		    if (g)
+				printf("X\n");
+		    else
+				printf("S\n");
+			printf("%d", economyclass); 
+			economyclass = economyclass - 1; 
+	    }
+	    if (seat_class == 3 && seat_number == 3)
+	    {
+		    if (h)
+				printf("X\n"); 
+		    else
+				printf("S\n"); 
+			printf("%d", economyclass); 
+			economyclass = economyclass - 1; 
+	    }
+	    if (seat_class == 3 && seat_number == 4) 
+	    { 
+		    if (i) 
+				printf("X\n");
+		    else 
+				printf("S\n");
+			printf("%d", economyclass); 
+			economyclass = economyclass - 1; 
+	    }
+	    if (seat_class == 3 && seat_number == 5)
+	    { 
+		    if (j) 
+				printf("X\n"); 
+		    else 
+				printf("S\n"); 
+			printf("%d", economyclass); 
+			economyclass = economyclass - 1; 
+	    }
+	    break;
+	}
 
-    bool is_occupied[3][5] = {};
-    for(int i = 2;i<5;i++) is_occupied[0][i] = 1;
-    for(int i = 3;i<5;i++) is_occupied[1][i] = 1;
-
-    int seat_left[3] = {2,3,5};
-    const int seat_count[3] = {2,3,5};
-    while(1) {
-        if(seat_count[0] == 0 && seat_count[1] == 0 && seat_count[2] == 0) {
-            printf("N\n");
-            return 0;
-        }
-
-        printf("請輸入艙等: ");
-        int tmp;
-        while(scanf("%d",&tmp)) {
-            if(tmp == -1) return 0;
-            if(tmp <= 0 || tmp > 3) {
-                printf("X\n");
-                continue;
-            }
-
-            if(seat_left[tmp] == 0) {
-                printf("X\n");
-                continue;
-            }
-
-            break;
-        }
-        int now = tmp - 1;
-
-        printf("請輸入座位: ");
-        while(scanf("%d",&tmp)) {
-            if(tmp <= 0 || tmp > seat_count[now]) {
-                printf("X\n");
-                continue;
-            }
-
-            break;
-        }
-
-        if(is_occupied[now][tmp - 1]) {
-            printf("F\n");
-            continue;
-        }
-
-        printf("S\n");
-        is_occupied[now][tmp - 1] = 1;
-    }
+	
+	
+	switch (seat_class) {
+	case 1:
+		if (firstclass == 0)
+			printf("F\n");
+		    break;
+	case 2:
+		if (businessclass == 0)
+			printf("F\n");
+		    break;
+	case 3:
+		if (economyclass == 0)
+			printf("F\n");
+		    break;
+	}
+	if (firstclass == 0 && businessclass == 0 && economyclass == 0)
+		printf("N\n");
+	system("pause");
+	return 0;
 }
